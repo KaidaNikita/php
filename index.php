8f87321448eca46547eead57e5e0eead282d57b1
Kaida <Kaida@rivne.itstep.org>
2019-12-03 18:52:53 +0200

    add captcha

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "_header.php"; ?>
+
+    <?php
+        echo "<h2> Hello</h2>";
+       // if (isset($_GET["email"]) and !empty($_GET["email"])) 
+        //echo $_GET["email"];
+    ?>
+
+<?php include "_footer.php"; ?>
